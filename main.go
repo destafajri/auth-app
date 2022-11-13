5f26cbae6ba374792e528693eea9d52f96251b50
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-11-13 10:49:23 +0700

    mono repo structure

--- main.go
+++ main.go
@@ -1,1 +1,7 @@
 package main
+
+import "github.com/destafajri/auth-app/routes"
+
+func main() {
+	routes.Router()
+}
--- services/auth-app/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/destafajri/auth-app/routes"
-)
-
-func main() {
-	routes.Router()
-}
